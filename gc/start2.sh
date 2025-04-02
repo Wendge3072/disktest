bafe2fde583a704c95229cfdbfb4c0a1b6d8a65e
Wendge3072 <wendge@outlook.com>
2025-04-02 22:41:30 +0800

    gc

--- gc/start2.sh
+++ gc/start2.sh
@@ -1,9 +1,19 @@
 date > time.txt
 fio seq.job
+
 date >> time.txt
-fio rnd.job
-date >> time.txt
-sleep 1800
-date >> time.txt
-fio rnd2.job
-echo "DONE" >> time.txt
+fio rnd.job &
+
+FIO_PID=$!
+echo "FIO started with PID: $FIO_PID"
+
+sleep 3600
+
+echo "Pausing FIO..."
+kill -STOP $FIO_PID
+
+echo "Resuming FIO..."
+kill -CONT $FIO_PID
+
+wait $FIO_PID
+echo "FIO finished."
