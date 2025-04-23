7657de33692ca6e2a61751dc4b2e6ca822bb01ec
Wendge3072 <wendge@outlook.com>
2025-04-23 19:28:19 +0800

    ftl

--- test/start.sh
+++ test/start.sh
@@ -1,7 +1,10 @@
 MAIN_PATH="/root/disktest/test/"
 
+echo "preparing" >> $MAIN_PATH/status
+fio $MAIN_PATH/seqwrite1M.job
+
 echo "start seq write" >> $MAIN_PATH/status
-fio $MAIN_PATH/seqwrite.job > fio.log 2>&1 &
+fio $MAIN_PATH/seqwrite64.job > fio.log 2>&1 &
 
 echo "start rnd read" >> $MAIN_PATH/status
 fio $MAIN_PATH/rndread.job
