6a44258a9356f7c18e0d9b785e92e14006b62be2
Wendge3072 <wendge@outlook.com>
2025-08-10 17:07:07 +0800

    xxx

--- nvmf/tcp/spdk_p4510/bandwidth/start64.sh
+++ nvmf/tcp/spdk_p4510/bandwidth/start64.sh
@@ -1,5 +1,27 @@
-date >> time.txt
-fio rnd64K.job
+#!/bin/bash
+time=$(date "+%Y%m%d_%H")
+filename=fio_${time}.log
 
-date >> time.txt
-fio rnd64K2.job
+echo '------------------------' >> ${filename}
+echo '     seq fio start!     ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio seq.job >> ${filename}
+
+echo '------------------------' >> ${filename}
+echo '   rnd64K_2 fio start!  ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio rnd64K_2.job >> ${filename}
+
+echo '------------------------' >> ${filename}
+echo '   rnd64K_3 fio start!  ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio rnd64K_3.job >> ${filename}
+
+echo '------------------------' >> ${filename}
+echo '   rnd64K_4 fio start!  ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio rnd64K_4.job >> ${filename}
