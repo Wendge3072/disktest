576f609c528bf82adaf00ee47fef01d373b136c7
Wendge3072 <wendge@outlook.com>
2025-08-11 16:13:34 +0800

    xxx

---
+++ nvmf/tcp/spdk_p4510/bandwidth/start16.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+time=$(date "+%Y%m%d_%H")
+filename=fio_${time}.log
+
+echo '------------------------' >> ${filename}
+echo '     seq fio start!     ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio seq.job >> ${filename}
+
+echo '------------------------' >> ${filename}
+echo '   rnd16K_2 fio start!  ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio rnd16K_2.job >> ${filename}
+
+echo '------------------------' >> ${filename}
+echo '   rnd16K_3 fio start!  ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio rnd16K_3.job >> ${filename}
+
+echo '------------------------' >> ${filename}
+echo '   rnd16K_4 fio start!  ' >> ${filename}
+echo '------------------------' >> ${filename}
+date >> ${filename}
+fio rnd16K_4.job >> ${filename}
