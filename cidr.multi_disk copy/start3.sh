f3f00e10499ca94a51e1c9b483209be0139faab6
Wendge3072 <wendge@outlook.com>
2025-09-24 22:40:13 +0800

    xxx

---
+++ cidr.multi_disk copy/start3.sh
@@ -0,0 +1,2 @@
+date >> time.txt
+fio rnd3.job
---
+++ gc_inf_onBW/start64.sh
@@ -0,0 +1,26 @@
+date > time.txt
+fio seq.job
+
+date >> time.txt
+fio rnd64.job
+
+date >> time.txt
+fio rnd64lim1.job
+
+date >> time.txt
+fio rnd64lim2.job
+
+date >> time.txt
+fio rnd64lim3.job
+
+date >> time.txt
+fio rnd64lim4.job
+
+date >> time.txt
+fio rnd64lim5.job
+
+date >> time.txt
+fio rnd64lim6.job
+
+date >> time.txt
+fio rnd64lim7.job
