44adc6a4c4dff9f63e4ffd70a6a76fcf22853637
Wendge3072 <wendge@outlook.com>
2025-10-07 18:19:56 +0800

    xxx

---
+++ gc_inf_onBW/start.sh
@@ -0,0 +1,5 @@
+date > time.txt
+fio rnd.job
+
+date >> time.txt
+fio rnd2.job
--- gc_inf_onBW/start64.sh
+++ gc_inf_onBW/start64.sh
@@ -1,26 +1,48 @@
 date > time.txt
-fio seq.job
+fio rnd.job
+
+date >> time.txt
+fio rnd2.job
+
+date >> time.txt
+fio rnd3.job
 
 date >> time.txt
 fio rnd64.job
 
+sleep 600
+
+echo "start limit jobs" >> time.txt
+
 date >> time.txt
 fio rnd64lim1.job
+
+sleep 300
 
 date >> time.txt
 fio rnd64lim2.job
 
+sleep 300
+
 date >> time.txt
 fio rnd64lim3.job
+
+sleep 300
 
 date >> time.txt
 fio rnd64lim4.job
 
+sleep 300
+
 date >> time.txt
 fio rnd64lim5.job
 
+sleep 300
+
 date >> time.txt
 fio rnd64lim6.job
+
+sleep 300
 
 date >> time.txt
 fio rnd64lim7.job
